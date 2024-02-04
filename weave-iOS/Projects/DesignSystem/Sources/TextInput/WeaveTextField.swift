3e9516ebaf92a0230edaa9d02f1c8d0793df3bef
Jisu Kim <jisu15.kim@gmail.com>
2024-02-04 18:01:38 +0900

    [WEAV-160] TextField 컴포넌트 구현

---
+++ weave-iOS/Projects/DesignSystem/Sources/TextInput/WeaveTextField.swift
@@ -0,0 +1,79 @@
+//
+//  WeaveTextField.swift
+//  DesignSystem
+//
+//  Created by Jisu Kim on 2/4/24.
+//
+
+import SwiftUI
+
+public struct WeaveTextField: View {
+    //MARK: - Properties
+    @Binding var text: String
+    @FocusState private var isFocused: Bool
+    let placeholder: String
+    let textAlignment: TextAlignment
+    let backgroundColor: Color
+    let font: Font
+    let showClearButton: Bool
+    let keyboardType: UIKeyboardType
+    
+    var focusTintColor: Color {
+        return isFocused ? DesignSystem.Colors.white : DesignSystem.Colors.lightGray
+    }
+    
+    //MARK: - Initialize
+    public init(
+        text: Binding<String>,
+        placeholder: String,
+        font: Font = .pretendard(._500, size: 17),
+        textAlignment: TextAlignment = .leading,
+        backgroundColor: Color = DesignSystem.Colors.darkGray,
+        showClearButton: Bool = false,
+        keyboardType: UIKeyboardType = .default
+    ) {
+        self._text = text
+        self.placeholder = placeholder
+        self.textAlignment = textAlignment
+        self.backgroundColor = backgroundColor
+        self.font = font
+        self.showClearButton = showClearButton
+        self.keyboardType = keyboardType
+    }
+    
+    //MARK: - Body
+    public var body: some View {
+        HStack(spacing: 5) {
+            TextField(placeholder, text: $text)
+                .font(font)
+                .focused($isFocused)
+                .frame(height: 48)
+                .multilineTextAlignment(textAlignment)
+                .foregroundStyle(DesignSystem.Colors.white)
+                .padding(.vertical, 7)
+                .keyboardType(keyboardType)
+            
+            if showClearButton {
+                Button(action: {
+                    text = ""
+                }, label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .foregroundStyle(focusTintColor)
+                })
+            }
+        }
+        .padding(.horizontal, 16)
+        .background(backgroundColor)
+        .clipShape(
+            RoundedRectangle(cornerRadius: 10)
+        )
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .inset(by: 1)
+                .stroke(focusTintColor,
+                        lineWidth: 1
+                       )
+                .foregroundColor(.clear)
+        )
+    }
+}
