1226e95da99ce5b95340ebba50e32d2bb8288b8b
Jisu Kim <jisu15.kim@gmail.com>
2024-02-04 18:35:08 +0900

    [WEAV-160] UIWiondow Size 이슈, Keyboard hide 공통 메서드

---
+++ weave-iOS/Projects/App/Sources/SignUp/StepSubViews/SignUpMBTIView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpMBTIView.swift
+//  weave-ios
+//
+//  Created by Jisu Kim on 2/4/24.
+//
+
+import SwiftUI
+
+struct SignUpMBTIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpMBTIView()
+}
