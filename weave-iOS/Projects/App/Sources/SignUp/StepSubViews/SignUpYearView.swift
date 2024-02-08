372db0ded02c2fad27fa36f99b3cb9dd458a6fe1
Jisu Kim <jisu15.kim@gmail.com>
2024-02-09 02:01:33 +0900

    [FEAT] SignUp View TCA 구조로 Validation / Action 연결

--- weave-iOS/Projects/App/Sources/SignUp/StepSubViews/SignUpYearView.swift
+++ weave-iOS/Projects/App/Sources/SignUp/StepSubViews/SignUpYearView.swift
@@ -6,38 +6,56 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 import DesignSystem
 
 struct SignUpYearView: View {
-    @State var yearText = String()
+    let store: StoreOf<SignUpFeature>
     
     var body: some View {
-        VStack {
-            HStack(spacing: 12) {
-                WeaveTextField(
-                    text: $yearText,
-                    placeholder: "YYYY",
-                    font: .pretendard(._600, size: 20),
-                    textAlignment: .center,
-                    keyboardType: .numberPad
-                )
-                .frame(width: 100)
-                Text("년")
-                    .font(.pretendard(._600, size: 20))
+        WithViewStore(store, observe: { $0 }) { viewStore in
+            VStack {
+                HStack(spacing: 12) {
+                    WeaveTextField(
+                        text: viewStore.$birthYear,
+                        placeholder: "YYYY",
+                        font: .pretendard(._600, size: 20),
+                        textAlignment: .center,
+                        keyboardType: .numberPad
+                    )
+                    .frame(width: 100)
+                    Text("년")
+                        .font(.pretendard(._600, size: 20))
+                }
+                
+                Text("프로필에 표시되는 나이를 위한 정보 수집으로,\n회원가입 이후 변경할 수 없어요!")
+                    .font(.pretendard(._400, size: 14))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(DesignSystem.Colors.white)
+                    .padding(.vertical, 11)
+                
+                Spacer()
+                
+                WeaveButton(
+                    title: "다음으로",
+                    size: .medium,
+                    isEnabled: validateNextButton(year: viewStore.birthYear)
+                ) {
+                    viewStore.send(.didTappedNextButton)
+                }
+                .padding(.bottom, 20)
             }
-            
-            Text("프로필에 표시되는 나이를 위한 정보 수집으로,\n회원가입 이후 변경할 수 없어요!")
-                .font(.pretendard(._400, size: 14))
-                .multilineTextAlignment(.center)
-                .foregroundColor(DesignSystem.Colors.white)
-                .padding(.vertical, 11)
+            .onAppear(perform: {
+                UIApplication.shared.hideKeyboard()
+            })
         }
-        .onAppear(perform: {
-            UIApplication.shared.hideKeyboard()
-        })
     }
-}
-
-#Preview {
-    SignUpYearView()
+    
+    func validateNextButton(year: String) -> Bool {
+        guard year.count == 4,
+              Int(year) != nil else {
+            return false
+        }
+        return true
+    }
 }
