ccd6d92e020cb611b361bfb01da16a5c5edf4735
kangddong <rkdehddud96@naver.com>
2024-02-18 17:40:42 +0900

    [MOD] SecretKey add to Gitignore

---
+++ weave-iOS/Projects/App/Sources/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  weave-ios
+//
+//  Created by 강동영 on 2/18/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
