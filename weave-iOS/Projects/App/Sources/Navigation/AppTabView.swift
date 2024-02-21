43ecc0a9bb7cd272d482f41f3bd37eccad208491
KANG DONGYEONG <50406861+kangddong@users.noreply.github.com>
2024-02-21 13:47:32 +0900

    [WEAV-248] 홈 탭바 구현 (#18)
    * [ADD] 이미지 추가
    
    * [ADD] navigation &* apply ContentView, Connecct Mypage

---
+++ weave-iOS/Projects/App/Sources/Navigation/AppTabView.swift
@@ -0,0 +1,27 @@
+//
+//  AppTabView.swift
+//  weave-ios
+//
+//  Created by 강동영 on 2/21/24.
+//
+
+import SwiftUI
+
+struct AppTabView: View {
+    @Binding var selection: AppScreen?
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            ForEach(AppScreen.allCases) { screen in
+                screen.destination
+                    .tag(screen as AppScreen?)
+                    .tabItem { screen.label }
+            }
+        }
+        .tint(.white)
+    }
+}
+
+#Preview {
+    AppTabView(selection: .constant(.chat))
+}
