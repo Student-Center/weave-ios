983d093504af68a586b3bc1c1a9ccfb4daa6a1f6
KANG DONGYEONG <50406861+kangddong@users.noreply.github.com>
2024-02-21 19:09:25 +0900

    [WEAV-249] 로그인, 홈 분기 및 탭뷰 목업뷰 추가 (#19)
    * [FEAT] dummy View add  to TabView
    
    * [FEAT] AppEntry Login 여부에 따른 화면이동 분기 처리

---
+++ weave-iOS/Projects/App/Sources/MyTeam/MyTeamView.swift
@@ -0,0 +1,18 @@
+//
+//  MyTeamView.swift
+//  weave-ios
+//
+//  Created by 강동영 on 2/21/24.
+//
+
+import SwiftUI
+
+struct MyTeamView: View {
+    var body: some View {
+        Text("Hello, MyTeam!")
+    }
+}
+
+#Preview {
+    MyTeamView()
+}
