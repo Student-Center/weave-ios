a7befe23d1040ba3b9c148c3eb3397ba323364a3
Jisu Kim <jisu15.kim@gmail.com>
2024-03-25 16:26:59 +0900

    [WEAV-308] 불필요한 코드 제거

--- weave-iOS/Projects/App/Sources/KakaoShare/KakaoShareManager.swift
+++ weave-iOS/Projects/App/Sources/KakaoShare/KakaoShareManager.swift
@@ -12,11 +12,6 @@ import KakaoSDKTemplate
 import SafariServices
 
 class KakaoShareManager {
-    var safariViewController : SFSafariViewController? // to keep instance
-
-    let title = "[WEAVE] 친구야 이 팀 어때?"
-    let description = "피드 메시지 예제"
-
     static func getMeetingTeamShareMessage(teamId: String) -> Data {
         return """
                 {
@@ -25,8 +20,8 @@ class KakaoShareManager {
                         "title": "[WEAVE] 친구야 이 팀 어때?",
                         "image_url": "\(SecretKey.serverResourcePath)/share_image.png",
                         "link": {
-                            "mobile_web_url": "https://weave-server-dev-bucket.s3.ap-northeast-2.amazonaws.com/resource/share_image.png",
-                            "web_url": "https://weave-server-dev-bucket.s3.ap-northeast-2.amazonaws.com/resource/share_image.png"
+                            "mobile_web_url": "\(SecretKey.serverResourcePath)/share_image.png",
+                            "web_url": "\(SecretKey.serverResourcePath)/share_image.png"
                         },
                     },
                     "buttons": [
@@ -51,8 +46,8 @@ class KakaoShareManager {
                         "title": "[WEAVE] 친구야 같이 미팅하자",
                         "image_url": "\(SecretKey.serverResourcePath)/share_image.png",
                         "link": {
-                            "mobile_web_url": "https://weave-server-dev-bucket.s3.ap-northeast-2.amazonaws.com/resource/share_image.png",
-                            "web_url": "https://weave-server-dev-bucket.s3.ap-northeast-2.amazonaws.com/resource/share_image.png"
+                            "mobile_web_url": "\(SecretKey.serverResourcePath)/share_image.png",
+                            "web_url": "\(SecretKey.serverResourcePath)/share_image.png"
                         },
                     },
                     "buttons": [
@@ -91,18 +86,6 @@ class KakaoShareManager {
                     }
                 }
             }
-        } else {
-            // 카카오톡 미설치: 웹 공유 사용 권장
-            // Custom WebView 또는 디폴트 브라우져 사용 가능
-            // 웹 공유 예시 코드
-//            if let url = ShareApi.shared.makeDefaultUrl(templatable: templatable) {
-//                self.safariViewController = SFSafariViewController(url: url)
-//                self.safariViewController?.modalTransitionStyle = .crossDissolve
-//                self.safariViewController?.modalPresentationStyle = .overCurrentContext
-//                self.present(self.safariViewController!, animated: true) {
-//                    print("웹 present success")
-//                }
-//            }
         }
     }
 }
